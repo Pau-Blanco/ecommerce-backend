755b5f2370e22878f38ff03aba5b2b5334f5f7a8
Pau Blanco <paublaber@campus.monlau.com>
2025-10-10 23:32:49 +0200

    We add the schema of Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'stock',
+        'image_url',
+        'category_id',
+
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
