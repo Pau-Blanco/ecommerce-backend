0cf7bc8aa3ac4703a05ce201e925d8abc8b77a52
Pau Blanco <paublaber@campus.monlau.com>
2025-10-11 23:11:33 +0200

    add debug info

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -11,8 +11,15 @@ class UploadController extends Controller
 {
     public function store(Request $request)
     {
+        \Log::info('=== UPLOAD DEBUG START ===');
+        \Log::info('User ID: ' . $request->user()->id);
+        \Log::info('User role: ' . $request->user()->role);
+        \Log::info('Is admin: ' . ($request->user()->isAdmin() ? 'YES' : 'NO'));
+        \Log::info('Files received: ' . ($request->hasFile('image') ? 'YES' : 'NO'));
+
         // Verificar que el usuario es administrador
         if (!$request->user()->isAdmin()) {
+            \Log::warning('User is not admin - Blocking upload');
             return response()->json([
                 'message' => 'Unauthorized'
             ], Response::HTTP_FORBIDDEN);
@@ -24,10 +31,16 @@ class UploadController extends Controller
 
         if ($request->hasFile('image')) {
             $file = $request->file('image');
+            \Log::info('File details: ' . $file->getClientOriginalName() . ' - ' . $file->getSize() . ' bytes');
+
             $fileName = Str::random(20) . '.' . $file->getClientOriginalExtension();
             $path = $file->storeAs('products', $fileName, 'public');
 
             $url = Storage::disk('public')->url($path);
+
+            \Log::info('File stored at: ' . $path);
+            \Log::info('Generated URL: ' . $url);
+            \Log::info('=== UPLOAD DEBUG END - SUCCESS ===');
 
             return response()->json([
                 'url' => $url,
@@ -35,6 +48,7 @@ class UploadController extends Controller
             ], Response::HTTP_CREATED);
         }
 
+        \Log::warning('No file received in request');
         return response()->json([
             'message' => 'No image uploaded'
         ], Response::HTTP_BAD_REQUEST);
