3585d3f55684cb8c6606812da7d43a674ded7eb1
Pau Blanco <paublaber@campus.monlau.com>
2025-10-11 23:22:51 +0200

    add the schema for cart items table migration

--- database/migrations/2025_10_11_212121_create_cart_items_table.php
+++ database/migrations/2025_10_11_212121_create_cart_items_table.php
@@ -4,22 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('cart_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity')->default(1);
             $table->timestamps();
+
+            // Evitar duplicados en el mismo carrito
+            $table->unique(['cart_id', 'product_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('cart_items');
