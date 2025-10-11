79c6e4bf9a5ce2727653c6b10c6b6f971cf5e4e6
Pau Blanco <paublaber@campus.monlau.com>
2025-10-12 00:56:33 +0200

    add the schema of wishlist table migration

--- database/migrations/2025_10_11_225451_create_wishlists_table.php
+++ database/migrations/2025_10_11_225451_create_wishlists_table.php
@@ -4,22 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
+
+            // Evitar duplicados - un usuario no puede tener el mismo producto dos veces
+            $table->unique(['user_id', 'product_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('wishlists');
